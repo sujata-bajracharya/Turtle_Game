e30eb0734eb00ee7ca65602206b8bf05506d47d5
Sujata Bajracharya <sujatabajra3333@gmail.com>
2021-12-28 18:21:30 +0545

    Turtle race game Updated

--- turtle_game.py
+++ turtle_game.py
@@ -2,12 +2,12 @@ import turtle
 import random
 
 player_one = turtle.Turtle()
-player_one.color = "green"
+player_one.color("green")
 player_one.shape("turtle")
 player_one.penup()
 player_one.goto(-200,100)
 player_two = player_one.clone()
-player_two.color = "blue"
+player_two.color("blue")
 player_two.penup()
 player_two.goto(-200,-100)
 
